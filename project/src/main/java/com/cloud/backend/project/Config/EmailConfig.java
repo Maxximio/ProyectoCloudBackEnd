628f2faa695560fd5115959b9194285ebf3fcbd4
Carlos Montalvo <patriciosalas101@gmail.com>
2024-07-20 19:55:30 -0500

    intento de corregir error correo 1

--- project/src/main/java/com/cloud/backend/project/Config/EmailConfig.java
+++ project/src/main/java/com/cloud/backend/project/Config/EmailConfig.java
@@ -22,6 +22,16 @@ public class EmailConfig {
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com"); // Por facilidad se sugiere el servidor de gmail
         properties.put("mail.smtp.port", "587"); 
+
+        //
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.starttls.required", "true");
+        properties.put("mail.smtp.connectiontimeout", "5000");
+        properties.put("mail.smtp.timeout", "5000");
+        properties.put("mail.smtp.writetimeout", "5000");
+        properties.put("mail.debug", "true");
+        //
+
         return properties;
     }
 
